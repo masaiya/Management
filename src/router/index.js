0cb278b4a1c3ffdd9b7cd5b3fc5c3ec0297022aa
echomsy <1285432055@qq.com>
2020-03-03 17:03:51 +0800

    登陆功能

--- src/router/index.js
+++ src/router/index.js
@@ -1,29 +1,42 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Login from "../components/Login";
+import Home from "../components/Home";
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    redirect: '/login'
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/login',
+    component: Login
+  },
+  {
+    path: '/home',
+    component: Home
   }
-]
+];
 
 const router = new VueRouter({
   mode: 'history',
-  base: process.env.BASE_URL,
+  /*base: process.env.BASE_URL,*/
   routes
-})
+});
+// 挂载路由导航守卫
+router.beforeEach((to, from, next) => {
+  // to: 表示将要访问的路径
+  // from: 表示从哪个路径跳转而来
+  // next: 是一个函数，表示放行
+  // next() 放行    next('/login')   强制跳转到某个路径
+
+  if(to.path === '/login') return next();
+  // 获取token
+  const tokenStr = window.sessionStorage.getItem('token');
+  if(!tokenStr) return next('/login');
+  next();
+});
 
 export default router
